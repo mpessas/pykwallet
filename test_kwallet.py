1ddf37e482e35c80f0a42ea2435d7efd31eeffd3
Apostolos Mpessas <mpessas@gmail.com>
2010-07-31 19:49:36 +0300

    Add unittest.

---
+++ test_kwallet.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from kwallet import KWallet, EntryNotFoundError
+
+
+class TestKWallet(unittest.TestCase):
+
+    def setUp(self):
+        self.kw = KWallet("test_kwallet")
+        self.kw.open()
+        self.kw.set_folder('test_folder')
+
+    def tearDown(self):
+        self.kw.close()
+
+    def test_get_error(self):
+        entry = "test1"
+        self.assertRaises(EntryNotFoundError, self.kw.get, entry)
+
+    def test_get_success(self):
+        entry = "test2"
+        value = "test_value"
+        self.kw.set(entry, value)
+        self.assertEqual(self.kw.get(entry), value)
+
+    def test_set(self):
+        entry = "test"
+        value = "test_password"
+        self.kw.set(entry, value)
+
+
+if __name__ == '__main__':
+    unittest.main()
