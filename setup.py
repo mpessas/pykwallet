35adc429ee98b9503aea3ab49ad381ff2a597125
Apostolos Mpessas <mpessas@gmail.com>
2010-09-13 19:46:06 +0300

    Add test package to distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup (
     name = "pykwallet",
     version = "0.4",
-    packages = ["pykwallet", ],
+    packages = ["pykwallet", "pykwallet.test"],
     author = "Apostolos Bessas",
     author_email = "mpessas@gmail.com",
     description = "Module to store and retrieve data from KWallet.",
