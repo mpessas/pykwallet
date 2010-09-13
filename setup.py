32ad5fcba432c5143f69721519ddd90f2aa112da
Apostolos Mpessas <mpessas@gmail.com>
2010-09-13 19:17:38 +0300

    Write correct setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ from setuptools import setup, find_packages
 setup (
     name = "pykwallet",
     version = "0.3",
-    packages = find_packages(),
+    packages = ["pykwallet", ],
+    author = "Apostolos Bessas",
+    author_email = "mpessas@gmail.com",
+    description = "Module to store and retrieve data from KWallet.",
+    license = "GPL",
+    test_suite = "pykwallet/test/test_pykwallet",
+    requires = ["dbus", "scipy"],
 )
